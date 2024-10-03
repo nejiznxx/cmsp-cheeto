53251c43251d180c3856f2817b0fff1829c1c739
marcos10pc <30839781+marcos10pc@users.noreply.github.com>
2024-10-03 18:07:03 -0300

    ADICIONADO ESPERA no BACKGROUND ENTRE 1 - 5 SEGUNDOS ANTES DE ENVIAR

--- tampermonkey.js
+++ tampermonkey.js
@@ -128,21 +128,25 @@
 
                         console.log(`[DEBUG] Sending Answers... BODY: ${JSON.stringify(send_anwsers_body)}`);
 
-                        sendRequest("PUT", `https://edusp-api.ip.tv/tms/task/${id}/answer/${task_id}`, send_anwsers_body, (response) => {
-                            if (response.status !== 200) {
-                                alert(`[ERROR] An error occurred while sending the answers. RESPONSE: ${response.responseText}`);
-                            }
-                            console.log(`[DEBUG] Answers Sent! RESPONSE: ${response.responseText}`);
+                        let waitTime = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
 
-                            const watermark = document.querySelector('.MuiTypography-root.MuiTypography-body1.css-1exusee');
-                            if (watermark) {
-                                watermark.textContent = 'Made by marcos10pc :P';
-                                watermark.style.fontSize = '70px';
-                                setTimeout(() => {
-                                    document.querySelector('button.MuiButtonBase-root.MuiButton-root.MuiLoadingButton-root.MuiButton-contained.MuiButton-containedInherit.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-colorInherit.css-prsfpd').click();
-                                }, 500);
-                            }
-                        });
+                        setTimeout(() => {
+                            sendRequest("PUT", `https://edusp-api.ip.tv/tms/task/${id}/answer/${task_id}`, send_anwsers_body, (response) => {
+                                if (response.status !== 200) {
+                                    alert(`[ERROR] An error occurred while sending the answers. RESPONSE: ${response.responseText}`);
+                                }
+                                console.log(`[DEBUG] Answers Sent! RESPONSE: ${response.responseText}`);
+                            });
+                        }, waitTime)
+
+                        const watermark = document.querySelector('.MuiTypography-root.MuiTypography-body1.css-1exusee');
+                        if (watermark) {
+                            watermark.textContent = 'Made by marcos10pc :P';
+                            watermark.style.fontSize = '70px';
+                            setTimeout(() => {
+                                document.querySelector('button.MuiButtonBase-root.MuiButton-root.MuiLoadingButton-root.MuiButton-contained.MuiButton-containedInherit.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-colorInherit.css-prsfpd').click();
+                            }, 500);
+                        }
                     });
                 });
             }
